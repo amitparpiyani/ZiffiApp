61d65e2f3f3ab20590cc9e170f9fd5cbf6ab25e6
amit parpiyani <amit.parpiyanee@gmail.com>
2015-05-18 12:02:42 +0530

    Initial Commit

---
+++ Ziffi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ziffi
+//
+//  Created by Amit Parpiyani on 18/05/15.
+//  Copyright (c) 2015 DevPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ziffi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ziffi
+//
+//  Created by Amit Parpiyani on 18/05/15.
+//  Copyright (c) 2015 DevPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
